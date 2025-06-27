516364b8eb53301412e5160aea742e4c5f95a08f
Garefild <Garefild2@gmail.com>
2025-06-27 21:12:10 +0300

    main: cli => main

--- xbuild.config.ts
+++ xbuild.config.ts
@@ -32,7 +32,7 @@ const config: xBuildConfig[] = [
             external: [ './index.js' ],
             sourceRoot: `https://github.com/remotex-lab/xBuild/tree/v${ pkg.version }/`,
             entryPoints: {
-                cli: 'src/cli.ts',
+                main: 'src/main.ts',
                 index: 'src/index.ts'
             },
             loader: {
