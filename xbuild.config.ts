aeac14f3d30ff5c20d3a6a03b8a0aa4d3d439ec6
Garefild <Garefild2@gmail.com>
2025-06-27 21:14:31 +0300

    xbuild.config: refactor gitlab path

--- xbuild.config.ts
+++ xbuild.config.ts
@@ -30,7 +30,7 @@ const config: xBuildConfig[] = [
             packages: 'external',
             sourcemap: true,
             external: [ './index.js' ],
-            sourceRoot: `https://github.com/remotex-lab/xBuild/tree/v${ pkg.version }/`,
+            sourceRoot: `https://github.com/remotex-labs/xBuild/tree/v${ pkg.version }/`,
             entryPoints: {
                 main: 'src/main.ts',
                 index: 'src/index.ts'
