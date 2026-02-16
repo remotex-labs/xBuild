0b2555b15f268073ad2c4b54a412bbb56a08c717
Garefild <Garefild2@gmail.com>
2026-02-16 05:08:45 +0200

    vm-runtime.error: fix incorrect reference to `originalError` properties

--- src/errors/vm-runtime.error.ts
+++ src/errors/vm-runtime.error.ts
@@ -69,15 +69,15 @@ export class VMRuntimeError extends xBuildBaseError {
         super(originalError.message, 'VMRuntimeError');
 
         // Handle AggregateError
-        if (originalError instanceof AggregateError && Array.isArray(originalError.errors)) {
+        if (this.originalError instanceof AggregateError && Array.isArray(this.originalError.errors)) {
             // Process nested errors
-            this.errors = originalError.errors.map(error =>
+            this.errors = this.originalError.errors.map(error =>
                 new VMRuntimeError(error, options)
             );
         }
 
-        this.stack = originalError.stack;
-        this.message = originalError.message;
+        this.stack = this.originalError.stack;
+        this.message = this.originalError.message;
         this.reformatStack(originalError, options);
     }
 
