cb0b0165834c5bc971c86872a322067a4157f484
Garefild <Garefild2@gmail.com>
2026-02-19 00:25:49 +0200

    packages: update dependencies

---
+++ config.xbuild.ts
@@ -0,0 +1,44 @@
+/**
+ * Import will remove at compile time
+ */
+
+import type { xBuildConfig } from '@remotex-labs/xbuild';
+
+/**
+ * Imports
+ */
+
+import { version } from 'process';
+import pkg from './package.json' with { type: 'json' };
+
+/**
+ * Config build
+ */
+
+export const config: xBuildConfig = {
+    variants: {
+        main: {
+            define: {
+                __VERSION: pkg.version
+            },
+            esbuild: {
+                bundle: true,
+                minify: true,
+                format: 'esm',
+                target: [ `node${ version.slice(1) }` ],
+                platform: 'node',
+                packages: 'external',
+                sourcemap: true,
+                external: [ './index.js' ],
+                sourceRoot: `https://github.com/remotex-lab/xBuild/tree/v${ pkg.version }/`,
+                entryPoints: {
+                    bash: 'src/bash.ts',
+                    index: 'src/index.ts'
+                },
+                loader: {
+                    '.html': 'text'
+                }
+            }
+        }
+    }
+}
